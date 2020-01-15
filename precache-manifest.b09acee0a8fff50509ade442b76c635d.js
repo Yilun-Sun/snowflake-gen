a235fb5657a060c6b6b3741bf52efdb26ddbe9e1
Yilun-Sun <osirisnero@gmail.com>
2020-01-14 23:03:45 -0500

    Updates

---
+++ precache-manifest.b09acee0a8fff50509ade442b76c635d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd0b4b6bc9cb9e2e4b0807c45755facc",
+    "url": "/snowflake-gen/index.html"
+  },
+  {
+    "revision": "b9d5bed33a1ca5acc714",
+    "url": "/snowflake-gen/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "5e5dfabbc31a751e352d",
+    "url": "/snowflake-gen/static/js/2.bdb7624b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/snowflake-gen/static/js/2.bdb7624b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b9d5bed33a1ca5acc714",
+    "url": "/snowflake-gen/static/js/main.91bb8199.chunk.js"
+  },
+  {
+    "revision": "b3a28c162e5ca49df0c6",
+    "url": "/snowflake-gen/static/js/runtime-main.cbf00437.js"
+  }
+]);
